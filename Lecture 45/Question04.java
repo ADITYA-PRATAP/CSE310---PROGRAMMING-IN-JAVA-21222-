d3df644980a6538d5ae582fa16c4c536e507363e
Aditya Pratap Singh <72655723+ADITYA-PRATAP@users.noreply.github.com>
2022-03-20 16:14:56 +0530

    Update Question04.java

--- Lecture 45/Question04.java
+++ Lecture 45/Question04.java
@@ -3,7 +3,7 @@ public class TimingExample {
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 		int total = 0;
-		for (int i = 0; i < 10000; i++) {
+		for (int i = 0; i < 1000; i++) {
 			total = total + i;
 		}
 		long endTime = System.currentTimeMillis();
